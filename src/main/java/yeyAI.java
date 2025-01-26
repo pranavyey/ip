e43f7de2a361a37b3e25da576f8acffcc25beddd
Pranav <e1155906@u.nus.edu>
2025-01-26 13:41:00 +0800

    rename main class and add greeting

---
+++ src/main/java/yeyAI.java
@@ -0,0 +1,8 @@
+public class yeyAI {
+    public static void main(String[] args) {
+        String line = " ________________________\n";
+
+        System.out.println(line + "Hello! I'm yeyAI\n" + "What can I do for you?\n" + line
+                + "Bye. Hope to see you again soon!\n" + line);
+    }
+}
