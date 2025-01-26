13beaf1413332775e52a2c216839a2ce792364c5
pranavyey <e1155906@u.nus.edu>
2025-01-26 14:04:15 +0800

    Level-1 Echo

--- src/main/java/yeyAI.java
+++ src/main/java/yeyAI.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class yeyAI {
     public static void main(String[] args) {
-        String line = " ________________________\n";
+        String line = " ____________________________\n";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(line + "Hello! I'm yeyAI\n" + "What can I do for you?\n" + line
-                + "Bye. Hope to see you again soon!\n" + line);
+        System.out.println(line + "Hello! I'm yeyAI\n" + "What can I do for you?\n" + line);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line + input + "\n" + line);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
