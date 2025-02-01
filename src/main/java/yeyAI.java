4dc3cc608e7be25441ac1b2480ee2a4ee98770ad
Pranav <e1155906@u.nus.edu>
2025-02-01 21:39:49 +0800

    Level-6

--- src/main/java/yeyAI.java
+++ src/main/java/yeyAI.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class yeyAI {
     public static void main(String[] args) {
         String line = " ____________________________\n";
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int index = 1;
 
         System.out.println(line + "Hello! I'm yeyAI\n" + "What can I do for you?\n" + line);
@@ -15,20 +15,20 @@ public class yeyAI {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < index - 1; i++) {
                         int display = i + 1;
-                        System.out.printf("%d.%s\n", display, tasks[i].toString());
+                        System.out.printf("%d.%s\n", display, tasks.get(i).toString());
                     }
                     input = scanner.nextLine();
                 } else if (input.startsWith("mark")) { //mark branch
                     int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks[taskIndex].setDone();
+                    tasks.get(taskIndex).setDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(tasks[taskIndex].toString());
+                    System.out.println(tasks.get(taskIndex).toString());
                     input = scanner.nextLine();
                 } else if (input.startsWith("unmark")) { //unmark branch
                     int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks[taskIndex].setUndone();
+                    tasks.get(taskIndex).setUndone();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(tasks[taskIndex].toString());
+                    System.out.println(tasks.get(taskIndex).toString());
                     input = scanner.nextLine();
                 } else if (input.startsWith("todo")) { //new to-do task branch
                     String[] parts = input.split(" ", 2);
@@ -36,9 +36,9 @@ public class yeyAI {
                         throw new YeyException("Description of a todo cannot be empty. Try again with a description");
                     }
                     Task t = new Todo(parts[1]);
-                    tasks[index - 1] = t;
+                    tasks.add(t);
                     System.out.println(line + "Got it. I've added this task:");
-                    System.out.println(t.toString());
+                    System.out.println(t);
                     System.out.printf("Now you have %d tasks in the list.\n", index);
                     index++;
                     input = scanner.nextLine();
@@ -48,9 +48,9 @@ public class yeyAI {
                         throw new YeyException("Description of a deadline cannot be empty. Try again with a description");
                     }
                     Task t = new Deadline(parts[1]);
-                    tasks[index - 1] = t;
+                    tasks.add(t);
                     System.out.println(line + "Got it. I've added this task:");
-                    System.out.println(t.toString());
+                    System.out.println(t);
                     System.out.printf("Now you have %d tasks in the list.\n", index);
                     index++;
                     input = scanner.nextLine();
@@ -60,11 +60,22 @@ public class yeyAI {
                         throw new YeyException("Description of a event cannot be empty. Try again with a description");
                     }
                     Task t = new Deadline(parts[1]);
-                    tasks[index - 1] = t;
+                    tasks.add(t);
                     System.out.println(line + "Got it. I've added this task:");
-                    System.out.println(t.toString());
+                    System.out.println(t);
                     System.out.printf("Now you have %d tasks in the list.\n", index);
                     index++;
+                    input = scanner.nextLine();
+                } else if (input.startsWith("delete")) {
+                    String[] parts = input.split(" ", 2);
+                    if (parts.length < 2 || parts[1].trim().isEmpty()) {
+                        throw new YeyException("Specify the task to be deleted.");
+                    }
+                    if (Integer.parseInt(parts[1]) > index - 1) {
+                        throw new YeyException("Task to be deleted must exist within list");
+                    }
+                    tasks.remove(Integer.parseInt(parts[1]) - 1);
+                    index--;
                     input = scanner.nextLine();
                 } else {
                     throw new YeyException("Sorry, I don't know what that means. Try again");
