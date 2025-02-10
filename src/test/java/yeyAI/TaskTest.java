cee15c8541a115ce0142ee873c1ae4117994762a
pranavyey <e1155906@u.nus.edu>
2025-02-10 15:15:32 +0800

    A-JUnit

---
+++ src/test/java/yeyAI/TaskTest.java
@@ -0,0 +1,20 @@
+package yeyAI;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+
+    @Test
+    public void testTodoTaskDescription() {
+        Task task = new Todo("Complete CS2103");
+        assertEquals("Complete CS2103", task.getDescription(), "Task name should match");
+    }
+
+    @Test
+    public void testDeadlineTaskDescription() {
+        Task task = new Deadline("Complete CS2103 /by 2025-03-02");
+        assertEquals("Complete CS2103", task.getDescription(), "Task name should match");
+        task.setDone();
+        assertEquals("X", task.getStatusIcon(), "Task status should match");
+    }
+}
